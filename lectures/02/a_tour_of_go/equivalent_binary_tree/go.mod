666362485af07c774d38ef893f628fa6f9ea6cf1
rihib <rihib@rihib.dev>
2025-08-02 22:51:08 +0900

    create equivalent binary tree in a tour of go

---
+++ lectures/02/a_tour_of_go/equivalent_binary_tree/go.mod
@@ -0,0 +1,5 @@
+module github.com/rihib/mit6.5840/02/equivalent_binary_tree
+
+go 1.24.4
+
+require golang.org/x/tour v0.1.0
---
+++ lectures/02/a_tour_of_go/equivalent_binary_tree/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// https://go.dev/tour/concurrency/8
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	Walk(t.Left, ch)
+	ch <- t.Value
+	Walk(t.Right, ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go func() {
+		Walk(t1, ch1)
+		close(ch1)
+	}()
+	go func() {
+		Walk(t2, ch2)
+		close(ch2)
+	}()
+
+	for {
+		v1, ok1 := <-ch1
+		v2, ok2 := <-ch2
+		if ok1 != ok2 || v1 != v2 {
+			return false
+		}
+		if !ok1 {
+			break
+		}
+	}
+	return true
+}
+
+func main() {
+	if Same(tree.New(1), tree.New(1)) && !Same(tree.New(1), tree.New(2)) {
+		fmt.Println("Success")
+	}
+}
